f07449f891f20fc38e83a90d990be007a6a1d61b
wenqingyu <yuwenqingisu@gmail.com>
2017-05-08 18:17:12 +0800

    rate

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var hourly = '0 0 * * * *'
 var minutely = '0 * * * * *'
 
 // HeartBeatRate: 5 seconds
-var heartBeatRate = '* */10 * * * *'
+var heartBeatRate = '* */30 * * * *'
 
 console.log('Task Manager Start: ' + new Date().toISOString())
 
